970e091c840cae0210765020557eff734cac8d43
Lucas Homer <lucashomer@Lucass-MacBook-Pro.local>
2018-12-20 20:07:36 -0600

    add JWT to signup process

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,11 +1,12 @@
+const cookieParser = require('cookie-parser');
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
 
 const server = createServer();
 
-// TODO Use express middlware to handle cookies (JWT)
-// TODO Use express middlware to populate current user
+server.express.use(cookieParser());
+// TODO Use express middleware to populate current user
 
 server.start(
   {
