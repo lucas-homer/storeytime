c9ec7c16266953646baea45c83c728ca22b85d42
Lucas Homer <lucashomer@Lucass-MacBook-Pro.local>
2018-12-20 21:47:22 -0600

    shows currently logged in user

--- sick-fits/frontend/pages/signup.js
+++ sick-fits/frontend/pages/signup.js
@@ -5,12 +5,11 @@ const Columns = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
   grid-gap: 20px;
+  max-width: 40%;
 `;
 
 const SignupPage = props => (
   <Columns>
-    <Signup />
-    <Signup />
     <Signup />
   </Columns>
 );
