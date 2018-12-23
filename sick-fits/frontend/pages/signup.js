e76a34968aec7a822dd6879303c26cd956d57c9d
Lucas Homer <lucashomer@Lucass-MacBook-Pro.local>
2018-12-23 15:03:14 -0600

    Good through video 28

--- sick-fits/frontend/pages/signup.js
+++ sick-fits/frontend/pages/signup.js
@@ -1,16 +1,17 @@
 import Signup from '../components/Signup';
+import Signin from '../components/Signin';
 import styled from 'styled-components';
 
 const Columns = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
   grid-gap: 20px;
-  max-width: 40%;
 `;
 
 const SignupPage = props => (
   <Columns>
     <Signup />
+    <Signin />
   </Columns>
 );
 
