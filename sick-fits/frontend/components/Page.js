2ce3b6c1a17fa95b3580a094972ebbcfe270edef
Lucas Homer <lucashomer@Lucass-MacBook-Pro.local>
2019-02-24 11:55:38 -0600

    frontend password reset done

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,27 +1,27 @@
-import React from "react";
+import React from 'react';
 import Header from './Header';
 import Meta from './Meta';
 import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 
 const theme = {
-    red: '#FF0000',
-    black: '#393939',
-    grey: '#3A3A3A',
-    lightgrey: '#E1E1E1',
-    offWhite: '#EDEDED',
-    maxWidth: '1000px',
-    bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+  red: '#FF0000',
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightgrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
 };
 
 const StyledPage = styled.div`
-    background: white;
-    color: ${props => props.theme.black};
+  background: white;
+  color: ${props => props.theme.black};
 `;
 
 const Inner = styled.div`
-    max-width: ${props => props.theme.maxWidth};
-    margin: 0 auto;
-    padding: 2rem;
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
 `;
 
 injectGlobal`
@@ -51,18 +51,15 @@ injectGlobal`
   }
 `;
 export default class Page extends React.Component {
-    render() {
-        return (
-            <ThemeProvider theme={theme}>
-                <StyledPage>
-                    <Meta />
-                    <Header />
-                    <Inner>
-                    {this.props.children}
-                    </Inner>
-                </StyledPage>
-            </ThemeProvider>
-            
-        );
-    }
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Header />
+          <Inner>{this.props.children}</Inner>
+        </StyledPage>
+      </ThemeProvider>
+    );
+  }
 }
---
+++ sick-fits/frontend/pages/reset.js
@@ -0,0 +1,8 @@
+import Reset from '../components/Reset';
+
+const reset = props => (
+  <div>
+    <Reset resetToken={props.query.resetToken} />
+  </div>
+);
+export default reset;
